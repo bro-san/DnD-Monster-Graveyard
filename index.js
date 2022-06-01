b1226786900e86097bd6550f33bd86aa1795dd92
Joseph <josephputz89@gmail.com>
2022-06-01 16:43:46 -0500

    form and collapsible button

--- index.js
+++ index.js
@@ -1,66 +1,53 @@
 const submitForm = document.getElementById('monster-form')
 const monsterDiv = document.getElementById('monster-info')
 const monsterDescription = document.getElementById('description')
+const monsterButton = document.getElementsByClassName("collapsible");
 
+
+
+
+let i;
+for (i = 0; i < monsterButton.length; i++) {
+    monsterButton[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
 
 let monsterData = (monsterName) => {
     return fetch('http://localhost:3000/monsters/')
       .then(resp => resp.json())
       .then(data => data.forEach((element) => {
-        if (element.name == monsterName) {
+        if (element.name.toUpperCase() == monsterName.toUpperCase()) {
            //console.log(element)
            let monsterName = document.getElementById('monster-name')
            monsterName.innerText = element.name
-           //let monsterDescription = document.getElementById('description')
            monsterDescription.innerHTML = (`${element.meta}`) 
-           //<br><strong>Challenge:</strong> ${element.Challenge}`);
            let monsterImg = document.getElementById('monster-image')
            monsterImg.src = element.img_url
-           let monsterButton = document.getElementById('expanded-info-button')
-           monsterButton.addEventListener('click', (e) => {
-               //console.log(element)
-               if(monsterDescription.innerHTML == `${element.meta}`) {
-               //<br><strong>Challenge:</strong> ${element.Challenge}`)) {
-                   //console.log(monsterDescription.innerHTML)
-                   expandMonster(element)
-               }
-               else{
-                   console.log(monsterDescription.innerHTML)
-                   shrinkMonster(element)
-               } 
-               //<br><strong>Challenge:</strong> ${element.Challenge}`))
-               
-           })
+           let monsterDetails = document.getElementById('expanded-description')
+           monsterDetails.innerHTML = (`<strong>Armor Class:</strong> ${element['Armor Class']}
+           <br><strong>Hit Points:</strong> ${element['Hit Points']}
+           <br><strong>Speed:</strong> ${element.Speed}
+           <br><strong>Ability Scores</strong> <br>Strength: ${element.STR} <br>Dexterity: ${element.DEX} <br>Constitution: ${element.CON} <br>Intelligence: ${element.INT} <br>Wisdom: ${element.WIS} <br>Charisma: ${element.CHA}
+           <br><strong>Saving Throws:</strong> ${element['Saving Throws']}
+           <br><strong>Skills:</strong> ${element.Skills}
+           <br><strong>Senses:</strong> ${element.Senses}
+           <br><strong>Challenge:</strong> ${element.Challenge}
+           <br><strong>Traits:</strong> ${element.Traits}
+           <br><strong>Actions:</strong> ${element.Actions}
+           <br><strong>Legendary Actions:</strong> ${element['Legendary Actions']}
+           `)
 
         }
       }))
   }
 
-
-const expandMonster = (element) => {
-    //console.log(element)
-    //let newUl = document.createElement('ul')
-    //newUl = Object.assign({}, element)
-    //console.log(newUl)
-    monsterDescription.innerHTML = (`${element.meta}
-    <br><strong>Armor Class:</strong> ${element['Armor Class']}
-    <br><strong>Hit Points:</strong> ${element['Hit Points']}
-    <br><strong>Speed:</strong> ${element.Speed}
-    <br><strong>Ability Scores</strong> <br>Strength: ${element.STR} <br>Dexterity: ${element.DEX} <br>Constitution: ${element.CON} <br>Intelligence: ${element.INT} <br>Wisdom: ${element.WIS} <br>Charisma: ${element.CHA}
-    <br><strong>Saving Throws:</strong> ${element['Saving Throws']}
-    <br><strong>Skills:</strong> ${element.Skills}
-    <br><strong>Senses:</strong> ${element.Senses}
-    <br><strong>Challenge:</strong> ${element.Challenge}
-    <br><strong>Traits:</strong> ${element.Traits}
-    <br><strong>Actions:</strong> ${element.Actions}
-    <br><strong>Legendary Actions:</strong> ${element['Legendary Actions']}
-    `)
-    //alert(`${element}`)
-
-} 
-const shrinkMonster = (element) => {
-    monsterDescription.innerHTML = element.meta
-}
 
 
 submitForm.addEventListener("submit", (e) => {
