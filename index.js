e2cfc89a99f00c393688b06d1061fdcfde7d467f
Joseph <josephputz89@gmail.com>
2022-06-01 10:27:34 -0500

    filled out the fetch data

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-fetch('http://localhost:3000/monsters/')
-.then(resp => resp.json())
-.then(data => testFetch(data))
 
-function testFetch (data) {
-    console.log(data[0])
-}
+const submitForm = document.getElementById('monster-form')
+
+let monsterData = (monsterName) => {
+    return fetch('http://localhost:3000/monsters/')
+      .then(resp => resp.json())
+      .then(data => data.forEach((element) => {
+        if (element.name == monsterName) {
+           console.log(element)
+           let monsterName = document.getElementById('monster-name')
+           monsterName.innerText = element.name
+           let monsterDescription = document.getElementById('description')
+           monsterDescription.innerText = element.Traits
+           let monsterImg = document.getElementById('monster-image')
+           monsterImg.src = element.img_url
+           let monsterButton = document.getElementById('expanded-info-button')
+           monsterButton.addEventListener('click', (e) => {
+               console.log(element)
+           })
+        }
+      }))
+  }
+
+submitForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newMonster = submitForm['monster-search'].value
+    //console.log(newMonster)
+    monsterData(newMonster)
+    submitForm.reset();
+    //will reset the kill count when that's added
+})
