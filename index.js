40337375717efe3f3c79d447cfd34430116f3173
Joseph <josephputz89@gmail.com>
2022-06-01 09:52:42 -0500

    submit form

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-fetch('http://localhost:3000/monsters/')
-.then(resp => resp.json())
-.then(data => testFetch(data))
 
-function testFetch (data) {
-    console.log(data[0])
-}
+const submitForm = document.getElementById('monster-form')
+
+let monsterData = (monsterName) => {
+    return fetch('http://localhost:3000/monsters/')
+      .then(resp => resp.json())
+      .then(data => data.forEach((element) => {
+        if (element.name == monsterName) {
+           console.log(element)
+        }
+      }))
+  }
+
+submitForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newMonster = submitForm['monster-search'].value
+    //console.log(newMonster)
+    monsterData(newMonster)
+    submitForm.reset();
+    //will reset the kill count when that's added
+})
