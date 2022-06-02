f62135bf785238d9c94027700a55cce6e3769227
Brody S <102997790+bro-san@users.noreply.github.com>
2022-06-02 09:24:26 -0400

    Refactor index.js post collapse button (#7)
    * starting to refactor index.js to pull out the monster data
    
    * refactored index.js so randomizer works and search still works
    
    Co-authored-by: brody.sanford@gmail.com <brodysmac@brodens-mbp.lan>

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const monsterDescription = document.getElementById('description')
 const monsterButton = document.getElementsByClassName("collapsible");
 const killCount = document.getElementById('kill-count')
 
+var monsterData;
 
+fetch('http://localhost:3000/monsters/')
+  .then(res => res.json())
+  .then(data => monsterData = data)
 
 
 let i;
@@ -20,44 +24,32 @@ for (i = 0; i < monsterButton.length; i++) {
   });
 }
 
-let monsterData = (monsterName) => {
-    return fetch('http://localhost:3000/monsters/')
-      .then(resp => resp.json())
-      .then(data => data.forEach((element) => {
-        if (element.name.toUpperCase() == monsterName.toUpperCase()) {
-            displayMonster(element);
-        }
-      }))
+function displayMonster (element) {
+           let monsterName = document.getElementById('monster-name')
+           monsterName.innerText = element.name
+           monsterDescription.innerHTML = (`${element.meta}`) 
+           let monsterImg = document.getElementById('monster-image')
+           monsterImg.src = element.img_url
+           let monsterDetails = document.getElementById('expanded-description')
+           monsterDetails.innerHTML = (`<strong>Armor Class:</strong> ${element['Armor Class']}
+           <br><strong>Hit Points:</strong> ${element['Hit Points']}
+           <br><strong>Speed:</strong> ${element.Speed}
+           <br><strong>Ability Scores</strong> <br>Strength: ${element.STR} <br>Dexterity: ${element.DEX} <br>Constitution: ${element.CON} <br>Intelligence: ${element.INT} <br>Wisdom: ${element.WIS} <br>Charisma: ${element.CHA}
+           <br><strong>Saving Throws:</strong> ${element['Saving Throws']}
+           <br><strong>Skills:</strong> ${element.Skills}
+           <br><strong>Senses:</strong> ${element.Senses}
+           <br><strong>Challenge:</strong> ${element.Challenge}
+           <br><strong>Traits:</strong> ${element.Traits}
+           <br><strong>Actions:</strong> ${element.Actions}
+           <br><strong>Legendary Actions:</strong> ${element['Legendary Actions']}
+           `)
   }
-
-const displayMonster = (element) => {
-      //console.log(element)
-      let monsterName = document.getElementById('monster-name')
-      monsterName.innerText = element.name
-      monsterDescription.innerHTML = (`${element.meta}`) 
-      let monsterImg = document.getElementById('monster-image')
-      monsterImg.src = element.img_url
-      killCount.innerHTML = element.kill_count
-      let monsterDetails = document.getElementById('expanded-description')
-      monsterDetails.innerHTML = (`<strong>Armor Class:</strong> ${element['Armor Class']}
-      <br><strong>Hit Points:</strong> ${element['Hit Points']}
-      <br><strong>Speed:</strong> ${element.Speed}
-      <br><strong>Ability Scores</strong> <br>Strength: ${element.STR} <br>Dexterity: ${element.DEX} <br>Constitution: ${element.CON} <br>Intelligence: ${element.INT} <br>Wisdom: ${element.WIS} <br>Charisma: ${element.CHA}
-      <br><strong>Saving Throws:</strong> ${element['Saving Throws']}
-      <br><strong>Skills:</strong> ${element.Skills}
-      <br><strong>Senses:</strong> ${element.Senses}
-      <br><strong>Challenge:</strong> ${element.Challenge}
-      <br><strong>Traits:</strong> ${element.Traits}
-      <br><strong>Actions:</strong> ${element.Actions}
-      <br><strong>Legendary Actions:</strong> ${element['Legendary Actions']}
-      `)
-}
 
 submitForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    let newMonster = submitForm['monster-search'].value
-    //console.log(newMonster)
-    monsterData(newMonster)
+    let newMonster = submitForm['monster-search'].value;
+    monsterData.forEach(element => {if (element.name.toUpperCase() == newMonster.toUpperCase()) {displayMonster(element)}
+})
     submitForm.reset();
     //will reset the kill count when that's added
 })
@@ -66,9 +58,7 @@ submitForm.addEventListener("submit", (e) => {
 const randomMonsterBtn = document.querySelector('button#monster-randomizer')
 randomMonsterBtn.addEventListener('click', e => {
   let randomNum = Math.floor(Math.random() * 327)
-  return fetch('http://localhost:3000/monsters/')
-  .then(resp => resp.json())
-  .then(data => console.log(data[randomNum]))
+  displayMonster(monsterData[randomNum])
 })
 
 //adding persistent kill count function to kill button
